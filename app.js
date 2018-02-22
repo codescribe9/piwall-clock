bd27f1cf7725e3bba5357db0e5e82f646d1fef9c
Venkata Rapaka <vrapaka9@gmail.com>
2018-02-22 06:03:55 -0600

    ec5

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 
-let app = express();
+var app = express();
 app.use(express.static(__dirname + '/public'));
 
 app.get("/", (req, res) => {
